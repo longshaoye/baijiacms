bd32a4c080407ff51e7aef127e9259a25f496b96
jim-lung@163.com <1qaz2wsx>
2016-06-08 18:59:58 +0800

    整理首页等几个页面

--- install.php
+++ install.php
@@ -1,15 +1,20 @@
 <?php
+//如果已经安装，则跳转至首页
 if (file_exists(str_replace("\\", '/', dirname(__FILE__)) . '/config/install.link')) {
     header("location:index.php");
     exit();
 }
-define('LOCK_TO_INSTALL', true);
+
+define('LOCK_TO_INSTALL', true);//锁定安装状态
+
 $mod = 'site';
 $do = "install";
 $_GET['name'] = "public";
+
 ob_start();
 $CLASS_LOADER = "driver";
 require 'includes/init.php';
 ob_end_flush();
+
 exit();
 
