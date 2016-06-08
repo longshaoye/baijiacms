fd13a5ac88f16db16013a8b724a4ec94ec88f467
jim-lung@163.com <1qaz2wsx>
2016-06-08 16:11:13 +0800

    sssaas

--- install.php
+++ install.php
@@ -1,17 +1,15 @@
 <?php
-if(file_exists(str_replace("\\",'/', dirname(__FILE__)).'/config/install.link'))
-{
-			header("location:index.php");
-			
-	  exit;
+if (file_exists(str_replace("\\", '/', dirname(__FILE__)) . '/config/install.link')) {
+    header("location:index.php");
+    exit();
 }
-		define('LOCK_TO_INSTALL', true);	
-$mod='site';
-$do="install";
-$_GET['name']="public";
+define('LOCK_TO_INSTALL', true);
+$mod = 'site';
+$do = "install";
+$_GET['name'] = "public";
 ob_start();
-$CLASS_LOADER="driver";
+$CLASS_LOADER = "driver";
 require 'includes/init.php';
 ob_end_flush();
-exit;
+exit();
 
